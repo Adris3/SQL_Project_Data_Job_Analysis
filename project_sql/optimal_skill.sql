9bc1aa3ea8b600b4d333d98248f0716bf1657490
Advait Risbud <advaitris@example.com>
2026-06-25 17:40:05 -0400

    Repeated query for jobs in Toronto.

--- project_sql/optimal_skill.sql
+++ project_sql/optimal_skill.sql
@@ -4,6 +4,7 @@
 -- - This search will look at remote positions and positions in Toronto
 -- */
 
+-- REMOTE JOBS 
 WITH skills_demand AS (
     SELECT
         skills_dim.skill_id,
@@ -47,3 +48,52 @@ ORDER BY
     demand_count DESC,
     avg_salary DESC
 LIMIT 20;
+
+
+-- JOBS IN TORONTO
+
+WITH skills_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_location = 'Toronto, ON, Canada'
+    GROUP BY
+        skills_dim.skill_id
+), average_salary AS ( -- Looking at jobs in Toronto
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 2) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_location = 'Toronto, ON, Canada'
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE
+    demand_count > 10
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 20;
+
+-- There are no skills with a demand count greater than 100 in Toronto
