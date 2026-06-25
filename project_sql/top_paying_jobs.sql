c87e99b1d3cd2c5a4e21df4a93cf542e051aa89e
Advait Risbud <advaitris@example.com>
2026-06-25 13:14:48 -0400

    Added company name to the result

--- project_sql/top_paying_jobs.sql
+++ project_sql/top_paying_jobs.sql
@@ -13,6 +13,11 @@ SELECT
     job_posted_date
 FROM
     job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE 
     job_title_short = 'Data Engineer' AND
-    job_location = 'Anywhere';
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
