973090d9e7de5c09dd48fe36179007e4a7ce9b56
Advait Risbud <advaitris@example.com>
2026-06-25 13:51:14 -0400

    Created file for query 3

---
+++ project_sql/top_demanded_skills.sql
@@ -0,0 +1,7 @@
+/*
+What are the most in-demand skills for data engineers?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills  for a data engineer
+- Focus on all job postings (not just remote, or in Toronto)
+*/
+
