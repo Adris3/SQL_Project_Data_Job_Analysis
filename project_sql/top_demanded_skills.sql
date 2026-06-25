a8fa3dd9caf0b4beb32251aab43ff85a9cb3cbaa
Advait Risbud <advaitris@example.com>
2026-06-25 13:53:23 -0400

    Joined job postings to inner join like in the previous query. Did this to include the job skills in the result.

--- project_sql/top_demanded_skills.sql
+++ project_sql/top_demanded_skills.sql
@@ -5,3 +5,7 @@ What are the most in-demand skills for data engineers?
 - Focus on all job postings (not just remote, or in Toronto)
 */
 
+SELECT *
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
