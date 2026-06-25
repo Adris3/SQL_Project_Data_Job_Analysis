9cf2efb222666d8ea9552d1fba3a64a704db4ea6
Advait Risbud <advaitris@example.com>
2026-06-25 17:02:27 -0400

    Conducted the same query test for remote work, and jobs in Toronto

--- project_sql/top_paying_skills.sql
+++ project_sql/top_paying_skills.sql
@@ -18,3 +18,46 @@ GROUP BY
 ORDER BY
     avg_salary DESC
 LIMIT 5;
+
+-- REMOTE WORK
+
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg), 2)AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Engineer'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 5;
+
+-- TORONTO
+
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg), 2)AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Engineer'
+    AND salary_year_avg IS NOT NULL
+    AND job_location = 'Toronto, ON, Canada'
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 5;
+
+-- Insight
+/*
+- While SQL and Python are in the most demand, node and mongo are the highest paid
+- In remote work Assembly is now the highest paying skill, and Rust is now in the top 5
+- In Toronto, the highest paying skill is no-sql
+*/
