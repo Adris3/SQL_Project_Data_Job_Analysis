62a10f656f16bdabeaa834361138289d7426a2f1
Advait Risbud <advaitris@example.com>
2026-06-25 17:32:04 -0400

    Query now prints out 20 job skills showing the demand and average salary.

---
+++ project_sql/optimal_skill.sql
@@ -0,0 +1,44 @@
+-- /*
+-- Determine the most optimal skill to learn
+-- - Ideally this is the one that is in the highest demand, and has a high average salary
+-- - This search will look at remote positions and positions in Toronto
+-- */
+
+WITH skills_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY
+        skills_dim.skill_id
+), average_salary AS ( -- Looking at remote jobs first
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 2) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+LIMIT 20;
